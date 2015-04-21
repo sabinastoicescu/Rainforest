0391dad33a5d6479794ab70ffec81246eb0cb830
Sabina <sabina_stoicescu@yahoo.com>
2015-04-21 14:51:52 -0400

    Writing the review routes, controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
 
-  resources :products
+  resources :products do 
+  resources :reviews, only: [:show, :create, :destroy]
+
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
